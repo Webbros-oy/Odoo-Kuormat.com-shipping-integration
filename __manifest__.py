9bea039cf9abb0a12e8ed21fa89c08435bb83e1f
Pekka <82268651+PekkaTheWinter@users.noreply.github.com>
2026-06-24 17:36:53 +0300

    Documentation updated

---
+++ __manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Webbros Kuormat.com shipment integration',
+    'version': '19.0.1.0.1',
+    'category': 'Inventory/Delivery',
+    'summary': 'Kuormat.com shipment booking integration',
+    'author':  'Webbros',
+    'website': 'https://www.webbros.fi/r/Eyq',
+    'images':   ['static/description/Kuormat_com_shipment_integration.png'],
+
+
+    'description': """
+Delivery Kuormat.com Connector
+==========================
+This module serves as an integration for the Kuormat.com Delivery API.
+It extends `delivery.carrier` adding basic methods with webbros standard prefixes.
+    """,
+#    'depends': ['delivery', 'stock', 'sale_stock', 'purchase_stock', 'stock_dropshipping'],
+    'depends': ['delivery', 'stock', 'sale_stock', 'purchase_stock'],
+
+    'data': [
+        'security/ir.model.access.csv',
+        'data/kuormat_shipment_types_data.xml',
+        'views/delivery_carrier_views.xml',
+        'views/choose_delivery_carrier_views.xml',
+        'views/stock_picking_views.xml',
+        'wizard/wb_kuormat_book_wizard_views.xml',
+    ],
+    'installable': True,
+    'license': 'LGPL-3',
+}
