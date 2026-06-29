ae456b4bf0ab4282900d58597a2e22a7eb9d7bbc
PekkaTheWinter <pekka@winterisland.fi>
2026-06-29 13:51:11 +0300

    Update __manifest__.py

--- webbros_kuormat_connector/__manifest__.py
+++ webbros_kuormat_connector/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Webbros Kuormat.com shipment integration',
-    'version': '19.0.1.0.4',
+    'version': '19.0.1.0.5',
     'category': 'Inventory/Delivery',
     'summary': 'Kuormat.com shipment booking integration',
     'author':  'Webbros',
